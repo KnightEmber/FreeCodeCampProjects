4727ce9d39a02a0f9fa41d0a5a1ecb56505d7a7b
KnightEmber <AJRoger78@gmail.com>
2026-08-21 22:50:22 -0400

    added endpoint for build-a-timestamp microservice to get date/time

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -12,7 +12,24 @@ app.get("/", (_req, res) => {
 });
 
 // Do not change code above this line
+app.get(["/api", "/api/:date"], (req, res) => {
+  const dateParam = req.params.date;
 
+  const date = !dateParam
+    ? new Date()
+    : !isNaN(dateParam)
+    ? new Date(parseInt(dateParam))
+    : new Date(dateParam)
+
+    if (date.toString() == "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    }
+
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+});
 // Do not change code below this line
 
 const PORT = 8000;
